24673cbe21eef6851bc70240b16fd1d448a10cba
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2020-07-03 16:59:02 +0100

    add project controller, model and migration

---
+++ database/migrations/2020_07_03_154441_create_projects_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('title');
+            $table->string('intro');
+            $table->mediumText('body');
+            $table->string('github_ink');
+            $table->string('external_link');
+            $table->string('cover_image_link');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
