4727d342f9a3e20934e734ff7e8007db78769d4d
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2020-07-03 16:58:23 +0100

    add pages controller and route to welcome page

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\About;
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function welcome()
+    {
+        return view('welcome');
+    }
+}
