9a6167c178141bc28904518b42853b1a03b17ba3
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2020-07-08 08:42:56 +0100

    Add section for projects

--- resources/views/pages/projects/projects.blade.php
+++ resources/views/pages/projects/projects.blade.php
@@ -4,24 +4,69 @@
 <div id="main">
     <div class="inner">
 
-    <!-- Header -->
-        <header id="header">
-            <a href="index.html" class="logo"><strong>The Creative Coder</strong> by Jeremy Smith</a>
-            <ul class="icons">
-                <li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
-                <li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
-                <li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
-                <li><a href="#" class="icon brands fa-medium-m"><span class="label">Medium</span></a></li>
-            </ul>
-        </header>
+        <!-- Header -->
+        @include('partials/header')
 
-    <!-- Content -->
+        <!-- Content -->
         <section>
             <header class="main">
                 <h1>My recent projects</h1>
             </header>
             <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit.</p>
             <p>Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit tristique lorem ipsum dolor.</p>
+        </section>
+
+        <section>
+            <div class="posts">
+                <article><a class="image" style="cursor: pointer;"><img src="images/pic01.jpg" alt=""></a>
+                    <h3>Interdum aenean</h3>
+                    <p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
+                    <ul class="actions">
+                        <li><a class="button primary" style="cursor: pointer;">Read more</a></li>
+                        <li><a class="button" style="cursor: pointer;">Github</a></li>
+                    </ul>
+                </article>
+                <article><a class="image" style="cursor: pointer;"><img src="images/pic02.jpg" alt=""></a>
+                    <h3>Nulla amet dolore</h3>
+                    <p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
+                    <ul class="actions">
+                        <li><a class="button primary" style="cursor: pointer;">Read more</a></li>
+                        <li><a class="button" style="cursor: pointer;">Github</a></li>
+                    </ul>
+                </article>
+                <article><a class="image" style="cursor: pointer;"><img src="images/pic03.jpg" alt=""></a>
+                    <h3>Tempus ullamcorper</h3>
+                    <p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
+                    <ul class="actions">
+                        <li><a class="button primary" style="cursor: pointer;">Read more</a></li>
+                        <li><a class="button" style="cursor: pointer;">Github</a></li>
+                    </ul>
+                </article>
+                <article><a class="image" style="cursor: pointer;"><img src="images/pic04.jpg" alt=""></a>
+                    <h3>Sed etiam facilis</h3>
+                    <p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
+                    <ul class="actions">
+                        <li><a class="button primary" style="cursor: pointer;">Read more</a></li>
+                        <li><a class="button" style="cursor: pointer;">Github</a></li>
+                    </ul>
+                </article>
+                <article><a class="image" style="cursor: pointer;"><img src="images/pic05.jpg" alt=""></a>
+                    <h3>Feugiat lorem aenean</h3>
+                    <p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
+                    <ul class="actions">
+                        <li><a class="button primary" style="cursor: pointer;">Read more</a></li>
+                        <li><a class="button" style="cursor: pointer;">Github</a></li>
+                    </ul>
+                </article>
+                <article><a class="image" style="cursor: pointer;"><img src="images/pic06.jpg" alt=""></a>
+                    <h3>Amet varius aliquam</h3>
+                    <p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
+                    <ul class="actions">
+                        <li><a class="button primary" style="cursor: pointer;">Read more</a></li>
+                        <li><a class="button" style="cursor: pointer;">Github</a></li>
+                    </ul>
+                </article>
+            </div>
         </section>
     </div>
 </div>
